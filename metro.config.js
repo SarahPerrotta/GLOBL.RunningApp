97f209e3e924d4db64e0bcfcca820f256c3dc117
SarahPerrotta <122047244+SarahPerrotta@users.noreply.github.com>
2025-08-20 13:19:03 +0100

    chore: uncommented section in friend suggestion feature

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
+
+
+const config = getDefaultConfig(__dirname);
+
+// can add custom asset extensions here
+
+module.exports = config;
